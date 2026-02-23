da712c069bb2e7dd994b3819a11ecb3543c21132
anastasiiaskreka <skreka.nastya@gmail.com>
2026-02-23 16:44:40 +0200

    feat: init

---
+++ src/prettier.config.mjs
@@ -0,0 +1,11 @@
+export default {
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 100,
+    tabWidth: 2,
+    useTabs: false,
+    arrowParens: 'always',
+    bracketSpacing: true,
+    endOfLine: 'lf',
+  };
